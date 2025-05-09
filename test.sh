85fef4c8182558e316931412c3bdaefc6a5e345e
Nour Massri <mnmassri@mit.edu>
2025-05-09 00:49:13 +0000

    modified script

--- test.sh
+++ test.sh
@@ -1,45 +1,33 @@
   tinygo build -o hello.wasm -target wasi main.go
 
-curl --unix-socket /tmp/wasm-serverless.sock -X POST http://localhost/create \
+curl --unix-socket /tmp/wasm-serverless.sock -X POST http://localhost/init \
   -H "Content-Type: application/json" \
   -d '{
-      "module_path": "/home/mnm/work/wasmserverless/go/hello.wasm",
-    "env": {
-      "WORKER_ID": "1"
-    },
-    "args": ["map"]
+      "module_path": "/home/mnm/work/wasmserverless/go/hello.wasm"
   }'
 
 cargo build --target wasm32-wasip1
 
 wasmtime /home/mnm/work/wasmserverless/hello_wasm/target/wasm32-wasip1/debug/hello_wasm.wasm
-  curl --unix-socket /tmp/wasm-serverless.sock -X POST http://localhost/create \
+  curl --unix-socket /tmp/wasm-serverless.sock -X POST http://localhost/init \
   -H "Content-Type: application/json" \
   -d '{
-      "module_path": "/home/mnm/work/wasmserverless/hello_wasm/target/wasm32-wasip1/debug/hello_wasm.wasm",
-    "env": {
-      "WORKER_ID": "1"
-    },
-    "args": ["map"]
+      "module_path": "/home/mnm/work/wasmserverless/hello_wasm/target/wasm32-wasip1/release/hello_wasm.wasm"
   }'
 
-  wasm-opt -Oz /home/mnm/work/wasmserverless/hello_wasm/target/wasm32-wasip1/debug/hello_wasm.wasm  -o hello_optimized.wasm
+  wasm-opt -Oz /home/mnm/work/wasmserverless/hello_wasm/target/wasm32-wasip1/release/hello_wasm.wasm  -o hello_optimized.wasm
 
-  curl --unix-socket /tmp/wasm-serverless.sock -X POST http://localhost/create \
+  curl --unix-socket /tmp/wasm-serverless.sock -X POST http://localhost/init \
   -H "Content-Type: application/json" \
   -d '{
-      "module_path": "/home/mnm/work/wasmserverless/hello_wasm/hello_optimized.wasm",
-    "env": {
-      "WORKER_ID": "1"
-    },
-    "args": ["map"]
+      "module_path": "/home/mnm/work/wasmserverless/hello_wasm/hello_optimized.wasm"
   }'
 
 
   curl --unix-socket /tmp/wasm-serverless.sock -X POST http://localhost/run \
   -H "Content-Type: application/json" \
   -d '{
-    "instance_id": "6b6ebc29-cb7b-4a6a-871b-0ac6b0a73f76"
+    "instance_id": "1"
   }'
 
 wasm-objdump -x hello.wasm
